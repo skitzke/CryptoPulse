5cc721518fe6db3981ff515530abd2651907465d
skitzke <egslotz@gmail.com>
2025-09-02 14:54:55 +0200

    Update CryptoPulse.csproj with MAUI

--- obj/Debug/net9.0-ios/iossimulator-x64/CryptoPulse.AssemblyInfo.cs
+++ obj/Debug/net9.0-ios/iossimulator-x64/CryptoPulse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoPulse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+90261b8de43e0aa68084f894344c910307829f11")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoPulse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoPulse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
